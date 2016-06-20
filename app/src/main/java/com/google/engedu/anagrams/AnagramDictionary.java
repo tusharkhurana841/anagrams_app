39fc63618611363704edb9ddb87b1c2bfb13ea04
Tushar Khurana <tusharkhurana841@gmail.com>
2016-06-20 19:14:48 +0530

    isGoodWord() added

--- app/src/main/java/com/google/engedu/anagrams/AnagramDictionary.java
+++ app/src/main/java/com/google/engedu/anagrams/AnagramDictionary.java
@@ -1,11 +1,14 @@
-package com.google.engedu.anagrams;
+ package com.google.engedu.anagrams;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Random;
+        import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.InputStreamReader;
+        import java.util.ArrayList;
+        import java.util.Arrays;
+        import java.util.HashMap;
+        import java.util.HashSet;
+        import java.util.Random;
 
 public class AnagramDictionary {
 
@@ -13,17 +16,45 @@ public class AnagramDictionary {
     private static final int DEFAULT_WORD_LENGTH = 3;
     private static final int MAX_WORD_LENGTH = 7;
     private Random random = new Random();
+    private HashSet<String> wordSet = new HashSet<String>();
+    private ArrayList<String> wordList = new ArrayList<String>();
+    private HashMap<String,ArrayList<String>> lettersToWord = new HashMap<String,ArrayList<String>>();
 
     public AnagramDictionary(InputStream wordListStream) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(wordListStream));
         String line;
         while((line = in.readLine()) != null) {
             String word = line.trim();
+            wordSet.add(word);
+            wordList.add(word);
+
+            String key = alphaOrder(word);
+            }
+            if(lettersToWord.containsKey(key)){
+                lettersToWord.get(key).add(word);
+            }
+            else{
+                ArrayList<String> temp = new ArrayList<String>();
+                temp.add(word);
+                lettersToWord.put(key,temp);
+            }
+
+
         }
+    }
+    public String alphaOrder(String word)
+    {
+        char a[]=word.toCharArray();
+        Arrays.sort(a);
+        return  String.valueOf(a);
     }
 
     public boolean isGoodWord(String word, String base) {
-        return true;
+
+        if(wordSet.contains(word) && !word.contains(base))
+            return true;
+
+        return false;
     }
 
     public ArrayList<String> getAnagramsWithOneMoreLetter(String word) {
